2eaed523611ad86a11c2f0245dad314b5e1e57c3
Bencodes1 <256.ben@gmail.com>
2021-04-14 07:57:23 -0400

    added homepage to urls.py, changed color of sample img, set default lat/long on homepage

--- mapp/urls.py
+++ mapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name = 'index'),
+    path('', views.homepage, name = 'homepage'),
     path('image/', views.image_render, name='image'),
 ]
--- mapp/views.py
+++ mapp/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from PIL import Image
 
-def index(request):
-    return HttpResponse("WELCOME to mapper, friend")
+def homepage(request):
+    # return HttpResponse("WELCOME to mapper, friend")
+    return render(request, "mapp/homepage.html")
 
 def image_render(request):    
-    image = Image.new('RGB', (128, 128), 'green')
+    image = Image.new('RGB', (256,256), (150,120,182))
     response = HttpResponse(content_type='image/jpeg')
     image.save(response, "JPEG")
     return response
